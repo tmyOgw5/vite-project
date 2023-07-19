161391d04e7e329602df961b40bb6341ce2c0f38
TomoyaOgawa <5stream418@gmail.com>
2023-07-19 16:37:49 +0900

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">
       <h1>React TypeScript Template</h1>
+      <Item />
     </div>
   )
 }
---
+++ src/components/Item.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Item() {
+    return (
+        <div className="Item">
+            <h1>Item components</h1>
+        </div>
+    )
+}
+
+export default Item
