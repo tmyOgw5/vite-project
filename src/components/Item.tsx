af8a0d01850dd58a44b24cc5900e48cab81b2047
TomoyaOgawa <5stream418@gmail.com>
2023-07-27 11:16:07 +0900

    add useState

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,24 @@
+import React, {useState} from "react";
 import "./Item.css";
 import Card from "./Card";
-import React from "react";
 
 const Item = (props: any) => {
+
+    const [price, setPrice] = useState(props.price);
+
+
+    const clickHandler = () => {
+        setPrice(price + 1);
+        console.log("Clicked!");
+    }
+
     return (
         <Card className="Item">
             <h1>{props.name}</h1>
-            <h2>Price: ${props.price}</h2>
+            <h2>Price: ${price}</h2>
+            <button onClick={clickHandler}>Change prices</button>
         </Card>
-    )
+    );
 }
 
 export default Item
