f2c5e8d593cace77a0666435c2a4516a3febac3b
TomoyaOgawa <5stream418@gmail.com>
2023-07-30 15:15:35 +0900

    add stete

--- src/components/ItemForm.tsx
+++ src/components/ItemForm.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ItemForm.css';
 
 const ItemForm = () => {
+    const [enteredItem, setEnteredItem]= useState('');
+    const [enteredPrice, setEnteredPrice]= useState('');
+
+    const itemChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredItem(event.target.value);
+    }
+
+    const priceChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredPrice(event.target.value);
+    }
+
+
     return (
         <form>
             <div className='item-input'>
                 <label htmlFor="name">Item Name</label>
-                <input type="text" id="name" />
+                <input type="text" id="name" onChange={itemChangeHandler}/>
             </div>
             <div className='price-input'>
                 <label htmlFor="price">Item Price</label>
-                <input type="number" id="price" />
+                <input type="number" id="price" onChange={priceChangeHandler}/>
             </div>
             <button>Add Item</button>
         </form>
