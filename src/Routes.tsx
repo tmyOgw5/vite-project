6c90be05a2dd68c20d877a654465a394882301a7
TomoyaOgawa <5stream418@gmail.com>
2024-04-28 23:32:23 +0900

    add BlogPostPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { RouteObject } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
 import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
 
 const routes: RouteObject[] = [
   {
@@ -10,6 +10,7 @@ const routes: RouteObject[] = [
       { path: '/', element: <HomePage /> },
       { path: 'project', element: <ProjectPage /> },
       { path: 'blog', element: <BlogPage /> },
+      { path: 'blog/:id', element: <BlogPostPage /> },
     ],
   },
 ];
